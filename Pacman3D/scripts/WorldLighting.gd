23f0111637eaf2c15617e5427759c2e9afe78940
natosath <bartolvinkovic@gmail.com>
2022-12-17 16:51:02 +0100

    Refactored project structure of branch. Changed lightning behaivour from random to period. Added WorldLightning to Map scene

--- Pacman3D/scripts/WorldLighting.gd
+++ Pacman3D/scripts/WorldLighting.gd
@@ -3,7 +3,9 @@ extends WorldEnvironment
 
 # Declare member variables here. Examples:
 var rng = RandomNumberGenerator.new()
-var last_delta = 0
+var degree = 0
+var color_delta = 0.05
+var r = 0
 # var green = Color(0.28, 0.48, 0.21, 1)
 # var orange = Color(1.28, 0.48, 0.21, 1)
 # var spooky_red =  Color(1.28, 0.11, 0.15, 1)
@@ -16,11 +18,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if last_delta > 90:
-		var r = rng.randf_range(-0.02, 0.02)
-		self.get_environment().background_color = Color(0.13 + r, 0.02 + r, 0.23 + r, 1)
-		# self.get_environment().ambient_light_color  = Color(0.1 + r, 0.06 + r, 0.06 + r, 1)
-		get_node("DirectionalLight").light_color = Color(1.28 + r, 0.48 + r, 0.21 + r, 1)
-		last_delta = 0
-	else:
-		last_delta += 1
+	degree = degree % 360
+	r = sin(deg2rad(degree)) * color_delta
+	self.get_environment().background_color = Color(0.13 + r, 0.02 + r, 0.23 + r, 1)
+	# self.get_environment().ambient_light_color  = Color(0.1 + r, 0.06 + r, 0.06 + r, 1)
+	r *= 2
+	get_node("DirectionalLight").light_color = Color(1.28 + r, 0.48 + r, 0.21 + r, 1)
+	degree += 1
